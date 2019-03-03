b3244c5b8db80cb15864fa6afa2a96f65c02cc1c
Eder Marques <31988308+L1224rd@users.noreply.github.com>
2019-03-03 16:39:22 -0300

    Update gel.js

--- gel.js
+++ gel.js
@@ -15,3 +15,20 @@ gel.get = (url, callback) => {
   
   http.send();
 };
+
+gel.post = (url, data, callback) => {
+  const http = new XMLHttpRequest();
+  http.open('POST', url, true);
+  http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+  
+  http.onload = function () {
+    if (http.status >= 200 && http.status < 400) {
+      // Success!
+      callback(http.responseText);
+    } else {
+      callback('server-error');
+    }
+  };
+  
+  http.send(data);
+};
